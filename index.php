4f94f3bb9d85de31194e504eb41a9dc65987800b
Konstantin Surikov <surikov@gmail.com>
2019-01-31 22:03:47 +0300

    Работа с условиями

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks):?> checked <?php endif ?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -89,7 +89,20 @@ $show_complete_tasks = rand(0, 1);
                                 <span class="checkbox__text">Сделать главную страницу Дела в порядке</span>
                             </label>
                         </td>
+                        <?php if ($show_complete_tasks):?>
+                          <tr class="tasks__item task task--completed">
+                            <td class="task__select">
+                                <label class="checkbox task__checkbox">
+                                    <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                    <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                                </label>
+                            </td>
+                            <td class="task__date">10.10.2019</td>
 
+                            <td class="task__controls">
+                            </td>
+                        </tr>
+                        <?php endif ?>
                         <td class="task__file">
                             <a class="download-link" href="#">Home.psd</a>
                         </td>
