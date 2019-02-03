a7ac5635727e92bbc7d787ea5c30eaaabfd402b2
Konstantin Surikov <surikov@gmail.com>
2019-02-03 19:26:47 +0300

    Добавляет двумерный массив с информацией о задачах

--- index.php
+++ index.php
@@ -4,6 +4,44 @@ $show_complete_tasks = rand(0, 1);
 
 $categories = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"];
 
+$task_list = [
+    [
+        'title' => 'Собеседование в IT компании',
+        'due_date' => '01.12.2019',
+        'category' => 'Работа',
+        'completed' => false
+    ],
+    [
+        'title' => 'Выполнить тестовое задание',
+        'due_date' => '25.12.2019',
+        'category' => 'Работа',
+        'completed' => false
+    ],
+    [
+        'title' => 'Сделать задание первого раздела',
+        'due_date' => '21.12.2019',
+        'category' => 'Учеба',
+        'completed' => true
+    ],
+    [
+        'title' => 'Встреча с другом',
+        'due_date' => '22.12.2019',
+        'category' => 'Входящие',
+        'completed' => false
+    ],
+    [
+        'title' => 'Купить корм для кота',
+        'due_date' => null,
+        'category' => '	Домашние дела',
+        'completed' => false
+    ],
+    [
+        'title' => 'Заказать пиццу',
+        'due_date' => null,
+        'category' => '	Домашние дела',
+        'completed' => false
+    ],
+];
 
 ?>
 <!DOCTYPE html>
@@ -88,6 +126,8 @@ $categories = ["Входящие", "Учеба", "Работа", "Домашни
                 </div>
 
                 <table class="tasks">
+                <?php foreach($task_list as $task):?>
+                <?php endforeach?>
                     <tr class="tasks__item task">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
