fe5d6b01a515807c314fe3f56a814cce450aa5ba
Konstantin Surikov <surikov@gmail.com>
2019-02-10 20:45:46 +0300

    Создание схемы БД личного проекта

---
+++ schema.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE doingsdone
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+USE doingsdone;
+
+CREATE TABLE projects (
+  id  INT AUTO_INCREMENT PRIMARY KEY,
+  title CHAR(128) NOT NULL UNIQUE,
+  user_id INT NOT NULL UNIQUE
+);
+
+CREATE TABLE tasks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  due_date TIMESTAMP,
+  completion_time TIMESTAMP,
+  status BIT DEFAULT 0,
+  title CHAR(255) NOT NULL UNIQUE,
+  attachment CHAR(128),
+  user_id INT NOT NULL
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  email CHAR(128) NOT NULL UNIQUE,
+  name CHAR(128) NOT NULL,
+  password CHAR(64) NOT NULL
+);
