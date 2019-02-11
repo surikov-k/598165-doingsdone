951782eb7504741f219c5e45ec0914caccddaea6
Konstantin Surikov <surikov@gmail.com>
2019-02-11 09:22:39 +0300

    Исправляет замечания

--- schema.sql
+++ schema.sql
@@ -6,20 +6,26 @@ USE doingsdone;
 
 CREATE TABLE projects (
   id  INT AUTO_INCREMENT PRIMARY KEY,
-  title CHAR(128) NOT NULL UNIQUE,
-  user_id INT NOT NULL UNIQUE
+  title CHAR(128) NOT NULL,
+  user_id INT NOT NULL
 );
+
+CREATE INDEX p_user ON projects(user_id);
 
 CREATE TABLE tasks (
   id INT AUTO_INCREMENT PRIMARY KEY,
   create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   due_date TIMESTAMP,
   completion_time TIMESTAMP,
-  status BIT DEFAULT 0,
-  title CHAR(255) NOT NULL UNIQUE,
+  status BOOL DEFAULT 0,
+  title CHAR(255) NOT NULL,
   attachment CHAR(128),
   user_id INT NOT NULL
 );
+
+CREATE INDEX t_user ON tasks(user_id);
+CREATE INDEX t_due ON tasks(due_date);
+
 
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -28,3 +34,6 @@ CREATE TABLE users (
   name CHAR(128) NOT NULL,
   password CHAR(64) NOT NULL
 );
+
+CREATE INDEX u_email ON users(email);
+CREATE INDEX u_name ON users(name);
